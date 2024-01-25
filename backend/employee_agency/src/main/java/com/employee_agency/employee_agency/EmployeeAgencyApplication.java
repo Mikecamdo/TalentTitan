745b28cb0d7122c88b8681400a7194ab3f98fca1
Mikecamdo <mcdoherty@smu.edu>
2024-01-25 14:34:05 -0600

    Initialize Backend
    Initialized a Spring Boot app for our backend using Maven

---
+++ backend/employee_agency/src/main/java/com/employee_agency/employee_agency/EmployeeAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.employee_agency.employee_agency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAgencyApplication.class, args);
+	}
+
+}
---
+++ backend/employee_agency/src/test/java/com/employee_agency/employee_agency/EmployeeAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee_agency.employee_agency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

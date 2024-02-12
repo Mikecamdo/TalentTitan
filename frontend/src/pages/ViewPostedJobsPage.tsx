aff4379aae6fe82a67e8a3b483e753125fc63c43
Mikecamdo <mcdoherty@smu.edu>
2024-02-12 15:24:29 -0600

    Fix View Jobs Page
    Added the Job ID with a link to the ViewPostedJobs page

--- frontend/src/pages/ViewPostedJobsPage.tsx
+++ frontend/src/pages/ViewPostedJobsPage.tsx
@@ -4,6 +4,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Table from "react-bootstrap/Table";
 import { CustomSearchBar } from "../components/CustomSearchBar";
+import { Link } from "react-router-dom";
 
 
 export const ViewPostedJobsPage = () => {
@@ -22,6 +23,7 @@ export const ViewPostedJobsPage = () => {
                         <Table striped bordered hover>
                             <thead>
                                 <tr className="button-bg text-light">
+                                    <th className="button-bg text-light">Job ID</th>
                                     <th className="button-bg text-light">Position Title</th>
                                     <th className="button-bg text-light">Employer</th>
                                     <th className="button-bg text-light">Start/End Date</th>
@@ -43,16 +45,19 @@ export const ViewPostedJobsPage = () => {
                                 ))} */}
                                 {/*The following is dummy data:*/}
                                 <tr>
+                                    <td><Link to={"/job"} className="inherit-color">SFTWRE1</Link></td>
                                     <td>Software Engineer</td>
                                     <td>Amazon</td>
                                     <td>May 5 - May 21</td>
                                 </tr>
                                 <tr>
+                                    <td><Link to={"/job"} className="inherit-color">SFTWRE2</Link></td>
                                     <td>Software Engineer</td>
                                     <td>Walmart</td>
                                     <td>June 5 - June 6</td>
                                 </tr>
                                 <tr>
+                                    <td><Link to={"/job"} className="inherit-color">UIUX1</Link></td>
                                     <td>UI/UX Designer</td>
                                     <td>Walmart</td>
                                     <td>June 5 - June 6</td>

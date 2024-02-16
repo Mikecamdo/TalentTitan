7b6c2efe6ba3021be4b2173cd1886b2069787dd4
Mikecamdo <mcdoherty@smu.edu>
2024-02-16 01:52:15 -0600

    Redid Sign In Page
    Completely redesigned the sign in page using mantine (no bootstrap is used)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,16 +6,26 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import '@mantine/charts/styles.css';
+import '@mantine/notifications/styles.css';
+import '@mantine/code-highlight/styles.css';
+import '@mantine/tiptap/styles.css';
+import '@mantine/dropzone/styles.css';
+import '@mantine/carousel/styles.css';
+import '@mantine/spotlight/styles.css';
+import '@mantine/nprogress/styles.css';
+
 import { createTheme, MantineProvider } from '@mantine/core';
 
 const theme = createTheme({
   /** Put your mantine theme override here */
 });
 
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <MantineProvider>
     <React.StrictMode>

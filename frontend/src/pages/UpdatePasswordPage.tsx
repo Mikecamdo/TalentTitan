cecfa3884deb3c718648ca93be5da2649e7474a4
Mikecamdo <mcdoherty@smu.edu>
2024-02-17 15:23:54 -0600

    Update Password Logic
    Added requirements for the password the user enters

--- frontend/src/pages/UpdatePasswordPage.tsx
+++ frontend/src/pages/UpdatePasswordPage.tsx
@@ -1,37 +1,66 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useState, useEffect, useContext } from "react";
 import { UserContext } from "../App";
 import {
   TextInput,
-  Anchor,
   Paper,
   Title,
   Text,
   Container,
   Button,
-} from '@mantine/core';
-import classes from '../css_modules/SignInPage.module.css';
+} from "@mantine/core";
+import classes from "../css_modules/SignInPage.module.css";
 
-const formValues = {
-  newPassword: "",
-  confirmPassword: "",
-};
+const ALPHABET_REGEX = /[a-zA-Z]/;
+const NUMERIC_REGEX = /\d/;
+const SPECIAL_REGEX = /[!@#$%^&*()_+]/;
+const LENGTH_REGEX = /^.{8,}$/;
 
 export const UpdatePasswordPage = () => {
-
   const navigate = useNavigate();
 
   const [disableButton, setDisableButton] = useState(true);
-  const [values, setValues] = useState(formValues);
+
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [newPasswordError, setNewPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
 
   useEffect(() => {
-    if (values.newPassword && values.confirmPassword) {
-      setDisableButton(false);
-    } else {
+    if (!newPassword || !confirmPassword) {
       setDisableButton(true);
+    } else if (newPasswordError || confirmPasswordError) {
+      setDisableButton(true);
+    } else {
+      setDisableButton(false);
     }
-  }, [values]);
+  }, [newPasswordError, confirmPasswordError]);
+
+  useEffect(() => {
+    if (newPassword) {
+      if (!ALPHABET_REGEX.test(newPassword)) {
+        setNewPasswordError("Password must contain an alphabet character");
+      } else if (!NUMERIC_REGEX.test(newPassword)) {
+        setNewPasswordError("Password must contain a numeric character");
+      } else if (!SPECIAL_REGEX.test(newPassword)) {
+        setNewPasswordError("Password must contain a special character");
+      } else if (!LENGTH_REGEX.test(newPassword)) {
+        setNewPasswordError("Password must be at least 8 characters long");
+      } else {
+        setNewPasswordError("");
+      }
+    } else {
+      setNewPasswordError("");
+    }
+
+    if (confirmPassword === newPassword || !confirmPassword) {
+      setConfirmPasswordError("");
+    } else {
+      setConfirmPasswordError("Passwords don't match");
+    }
+  }, [newPassword, confirmPassword]);
 
   const userContext = useContext(UserContext);
   const setCurrentUser = userContext?.setCurrentUser;
@@ -52,41 +81,47 @@ export const UpdatePasswordPage = () => {
       </Title>
 
       <Text c="dimmed" size="sm" ta="center" mt={5}>
-        Since this is your first time logging in, we ask that you reset your password for security purposes
+        Since this is your first time logging in, we ask that you reset your
+        password for security purposes
       </Text>
 
       <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput required
-                   type="password"
-                   label="New Password"
-                   placeholder="Enter new password"
-                   value={values.newPassword}
-                   onChange={(delta) => {
-                    setValues({ ...values, newPassword: delta.target.value });
-                   }}
+        <TextInput
+          required
+          type="password"
+          label="New Password"
+          placeholder="Enter new password"
+          value={newPassword}
+          onChange={(delta) => {
+            setNewPassword(delta.target.value);
+          }}
+          error={newPasswordError}
         />
 
-        <TextInput required
-                   type="password"
-                   mt="md"
-                   label="Confirm Password"
-                   placeholder="Confirm new password"
-                   value={values.confirmPassword}
-                   onChange={(delta) => {
-                    setValues({ ...values, confirmPassword: delta.target.value });
-                   }}
+        <TextInput
+          required
+          type="password"
+          mt="md"
+          label="Confirm Password"
+          placeholder="Confirm new password"
+          value={confirmPassword}
+          onChange={(delta) => {
+            setConfirmPassword(delta.target.value);
+          }}
+          error={confirmPasswordError}
         />
 
-        <Button fullWidth 
-                mt="xl"
-                disabled={disableButton}
-                onClick={() => {
-                  updatePassword();
-                }}
-                >
+        <Button
+          fullWidth
+          mt="xl"
+          disabled={disableButton}
+          onClick={() => {
+            updatePassword();
+          }}
+        >
           Update password
         </Button>
       </Paper>
     </Container>
   );
-}
+};

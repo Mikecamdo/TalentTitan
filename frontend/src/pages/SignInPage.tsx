ab9846e04989f76fe53f1745a0c630b3249665cb
Mikecamdo <mcdoherty@smu.edu>
2024-02-20 23:41:27 -0600

    Sign In Validation
    Added frontend validation to prevent the user from signing in with invalid credentials

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -10,28 +10,73 @@ import {
   Text,
   Container,
   Button,
-} from '@mantine/core';
-import classes from '../css_modules/SignInPage.module.css';
+} from "@mantine/core";
+import classes from "../css_modules/SignInPage.module.css";
 
 const formValues = {
   username: "",
   password: "",
 };
 
-export const SignInPage = () => {
+const STARTS_WITH_ALPHABET_REGEX = /^[a-zA-Z]/;
+const LENGTH_REGEX = /^.{8,}$/;
 
+const ALPHABET_REGEX = /[a-zA-Z]/;
+const NUMERIC_REGEX = /\d/;
+const SPECIAL_REGEX = /[!@#$%^&*()_+]/;
+
+export const SignInPage = () => {
   const navigate = useNavigate();
 
   const [disableButton, setDisableButton] = useState(true);
   const [values, setValues] = useState(formValues);
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   useEffect(() => {
-    if (values.username && values.password) {
+    if (
+      values.username &&
+      values.password &&
+      !usernameError &&
+      !passwordError
+    ) {
       setDisableButton(false);
     } else {
       setDisableButton(true);
     }
-  }, [values]);
+  }, [values, usernameError, passwordError]);
+
+  useEffect(() => {
+    if (values.username) {
+      if (
+        !STARTS_WITH_ALPHABET_REGEX.test(values.username) ||
+        !LENGTH_REGEX.test(values.username)
+      ) {
+        setUsernameError("Invalid username");
+      } else {
+        setUsernameError("");
+      }
+    } else {
+      setUsernameError("");
+    }
+  }, [values.username]);
+
+  useEffect(() => {
+    if (values.password) {
+      if (
+        !ALPHABET_REGEX.test(values.password) ||
+        !NUMERIC_REGEX.test(values.password) ||
+        !SPECIAL_REGEX.test(values.password) ||
+        !LENGTH_REGEX.test(values.password)
+      ) {
+        setPasswordError("Invalid password");
+      } else {
+        setPasswordError("");
+      }
+    } else {
+      setPasswordError("");
+    }
+  }, [values.password]);
 
   const userContext = useContext(UserContext);
   const setCurrentUser = userContext?.setCurrentUser;
@@ -55,41 +100,46 @@ export const SignInPage = () => {
         Welcome back!
       </Title>
       <Text c="dimmed" size="sm" ta="center" mt={5}>
-        Don't have an account?{' '}
+        Don't have an account?{" "}
         <Anchor size="sm" component="button">
           <Link to="/signUp">Sign up</Link>
         </Anchor>
       </Text>
 
       <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput label="Username"
-                   placeholder="Enter username"
-                   value={values.username}
-                   onChange={(delta) => {
-                    setValues({ ...values, username: delta.target.value });
-                   }}
+        <TextInput
+          label="Username"
+          placeholder="Enter username"
+          value={values.username}
+          onChange={(delta) => {
+            setValues({ ...values, username: delta.target.value });
+          }}
+          error={usernameError}
         />
 
-        <TextInput type="password"
-                   mt="md"
-                   label="Password"
-                   placeholder="Enter password"
-                   value={values.password}
-                   onChange={(delta) => {
-                    setValues({ ...values, password: delta.target.value });
-                   }}
+        <TextInput
+          type="password"
+          mt="md"
+          label="Password"
+          placeholder="Enter password"
+          value={values.password}
+          onChange={(delta) => {
+            setValues({ ...values, password: delta.target.value });
+          }}
+          error={passwordError}
         />
 
-        <Button fullWidth 
-                mt="xl"
-                disabled={disableButton}
-                onClick={() => {
-                  login();
-                }}
-                >
+        <Button
+          fullWidth
+          mt="xl"
+          disabled={disableButton}
+          onClick={() => {
+            login();
+          }}
+        >
           Sign in
         </Button>
       </Paper>
     </Container>
   );
-}
+};

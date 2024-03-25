ed8eeae41670633f23deba62f1e0b07b807ea5d4
Mikecamdo <mcdoherty@smu.edu>
2024-03-25 14:12:41 -0500

    Add Staff Member
    Added backend logic so that staff members are added to both the Staff and Users tables when being created

--- backend/src/main/java/com/employee_agency/employee_agency/services/StaffService.java
+++ backend/src/main/java/com/employee_agency/employee_agency/services/StaffService.java
@@ -2,9 +2,10 @@ package com.employee_agency.employee_agency.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.employee_agency.employee_agency.entities.Staff;
+import com.employee_agency.employee_agency.entities.User;
 import com.employee_agency.employee_agency.repositories.StaffRepository;
+import com.employee_agency.employee_agency.repositories.UserRepository;
 
 @Service
 public class StaffService {
@@ -12,7 +13,19 @@ public class StaffService {
     @Autowired
     private StaffRepository staffRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     public void createStaff(Staff staff) {
+        User newUser = new User();
+        newUser.setUsername(staff.getUsername());
+        newUser.setPassword("random"); //TODO: make this actually random
+        newUser.setFirstLogin(true);
+        newUser.setIsActive(true);
+        newUser.setUserType("staff");
+
+        userRepository.save(newUser);
+
         staffRepository.save(staff);
     }
 

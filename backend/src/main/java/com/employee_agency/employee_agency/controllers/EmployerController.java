04c36f107cd38da41a4279899ff4d0226e0bbeee
Mikecamdo <mcdoherty@smu.edu>
2024-03-25 01:14:54 -0500

    Updating Employers/Professionals
    Can now update employers and professionals in the database

--- backend/src/main/java/com/employee_agency/employee_agency/controllers/EmployerController.java
+++ backend/src/main/java/com/employee_agency/employee_agency/controllers/EmployerController.java
@@ -2,11 +2,13 @@ package com.employee_agency.employee_agency.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import com.employee_agency.employee_agency.entities.Employer;
+import com.employee_agency.employee_agency.entities.Professional;
 import com.employee_agency.employee_agency.services.EmployerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -22,4 +24,9 @@ public class EmployerController {
         return ResponseEntity.ok("Employer registered successfully");
     }
     
+    @PutMapping("/update")
+    public ResponseEntity<String> updateEmployer(@RequestBody Employer employer) {
+        employerService.updateEmployer(employer);
+        return ResponseEntity.ok("Employer updated successfully");
+    }
 }

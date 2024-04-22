9c4625a254c789d46f6c5fe5f9e3da0c5b758cd8
drewstew2021 <drewstewart@smu.edu>
2024-04-22 15:42:34 -0500

    added basic balance API

--- frontend/src/api/balancesApi.ts
+++ frontend/src/api/balancesApi.ts
@@ -3,25 +3,33 @@ import axios from 'axios';
 const apiEndpoint = '//localhost:8080'
 
 export const getBalance = (userId: any) => new Promise ((resolve, reject) => {
-    axios.get(`${apiEndpoint}/balance`, userId)
+    axios.get(`${apiEndpoint}/balances/get-by-user`, userId)
     .then(response => resolve(response.data))
     .catch(error => {
         resolve(error.response.data);
     });
 });
 
-export const updateBalance = (userId: any) => new Promise ((resolve, reject) => {
-    axios.post(`${apiEndpoint}/balance`, userId)
+export const updateBalance = (balanceId: any) => new Promise ((resolve, reject) => {
+    axios.post(`${apiEndpoint}/balances/update`, balanceId)
     .then(response => resolve(response.data))
     .catch(error => {
         resolve(error.response.data);
     });
 });
 
-// export const getBalance = (userId: any) => new Promise ((resolve, reject) => {
-//     axios.get(`${apiEndpoint}/balance`, userId)
-//     .then(response => resolve(response.data))
-//     .catch(error => {
-//         resolve(error.response.data);
-//     });
-// });
+export const addBalance = (balanceId: any) => new Promise ((resolve, reject) => {
+    axios.post(`${apiEndpoint}/balances/add`, balanceId)
+    .then(response => resolve(response.data))
+    .catch(error => {
+        resolve(error.response.data);
+    });
+});
+
+export const payBalance = (balanceId: any) => new Promise ((resolve, reject) => {
+    axios.post(`${apiEndpoint}/balances/pay`, balanceId)
+    .then(response => resolve(response.data))
+    .catch(error => {
+        resolve(error.response.data);
+    });
+});
--- frontend/src/api/professionalApi.ts
+++ frontend/src/api/professionalApi.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiEndpoint = '//localhost:8080'
 
 export const getProfessionalInfo = (professionalId: any) => new Promise ((resolve, reject) => {
-    axios.get(`${apiEndpoint}/users/professional`, professionalId)
+    axios.get(`${apiEndpoint}/users/professional/getProfessional`, professionalId)
     .then(response => resolve(response.data))
     .catch(error => {
         resolve(error.response.data);

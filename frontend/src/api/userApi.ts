709a59ccc9ed3dd30f993dcc9ec67f7a3a013328
drewstew2021 <drewstewart@smu.edu>
2024-04-22 16:04:03 -0500

    updated users api

--- frontend/src/api/userApi.ts
+++ frontend/src/api/userApi.ts
@@ -9,3 +9,27 @@ export const attemptSignIn = (loginRequest: any) => new Promise ((resolve, rejec
         resolve(error.response.data);
     });
 });
+
+export const updatePassword = (loginRequest: any) => new Promise ((resolve, reject) => {
+    axios.post(`${apiEndpoint}/users/update-password`, loginRequest)
+    .then(response => resolve(response.data))
+    .catch(error => {
+        resolve(error.response.data);
+    });
+});
+
+export const updateUser = (user: any) => new Promise ((resolve, reject) => {
+    axios.post(`${apiEndpoint}/users/update`, user)
+    .then(response => resolve(response.data))
+    .catch(error => {
+        resolve(error.response.data);
+    });
+});
+
+export const attemptRegister = (user: any) => new Promise ((resolve, reject) => {
+    axios.post(`${apiEndpoint}/users/register`, user)
+    .then(response => resolve(response.data))
+    .catch(error => {
+        resolve(error.response.data);
+    });
+});

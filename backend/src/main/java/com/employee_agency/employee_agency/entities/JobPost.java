1d177355bab0d5b6642e716520b7b637a8792592
Mikecamdo <mcdoherty@smu.edu>
2024-04-22 16:02:42 -0500

    Code Cleanup
    Cleaned up the code so it looks nicer

--- backend/src/main/java/com/employee_agency/employee_agency/entities/JobPost.java
+++ backend/src/main/java/com/employee_agency/employee_agency/entities/JobPost.java
@@ -13,90 +13,102 @@ public class JobPost {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String employerId;
+    private String jobName;
+    private String companyJobId;
+    private String contactFirstName;
+    private String contactLastName;
+    private String contactPhone;
+    private String contactEmail;
+    private String startDate;
+    private String endDate;
+    private String startTime;
+    private String endTime;
+    private String hourlyRate;
+
     public Long getId() {
         return id;
     }
     public void setId(Long id) {
         this.id = id;
     }
-    private String employerId;
+
     public String getEmployerId() {
         return employerId;
     }
     public void setEmployerId(String employerId) {
         this.employerId = employerId;
     }
-    private String jobName;
+
     public String getJobName() {
         return jobName;
     }
     public void setJobName(String jobName) {
         this.jobName = jobName;
     }
-    private String companyJobId;
+
     public String getCompanyJobId() {
         return companyJobId;
     }
     public void setCompanyJobId(String companyJobId) {
         this.companyJobId = companyJobId;
     }
-    private String contactFirstName;
+
     public String getContactFirstName() {
         return contactFirstName;
     }
     public void setContactFirstName(String contactFirstName) {
         this.contactFirstName = contactFirstName;
     }
-    private String contactLastName;
+
     public String getContactLastName() {
         return contactLastName;
     }
     public void setContactLastName(String contactLastName) {
         this.contactLastName = contactLastName;
     }
-    private String contactPhone;
+
     public String getContactPhone() {
         return contactPhone;
     }
     public void setContactPhone(String contactPhone) {
         this.contactPhone = contactPhone;
     }
-    private String contactEmail;
+
     public String getContactEmail() {
         return contactEmail;
     }
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
     }
-    private String startDate;
+
     public String getStartDate() {
         return startDate;
     }
     public void setStartDate(String startDate) {
         this.startDate = startDate;
     }
-    private String endDate;
+
     public String getEndDate() {
         return endDate;
     }
     public void setEndDate(String endDate) {
         this.endDate = endDate;
     }
-    private String startTime;
+
     public String getStartTime() {
         return startTime;
     }
     public void setStartTime(String startTime) {
         this.startTime = startTime;
     }
-    private String endTime;
+
     public String getEndTime() {
         return endTime;
     }
     public void setEndTime(String endTime) {
         this.endTime = endTime;
     }
-    private String hourlyRate;
 
     public String getHourlyRate() {
         return hourlyRate;
